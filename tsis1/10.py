07bd5b6f6321715c5fad6d7a531a0121afe8a0be
Adishkaboii <123965200+Adishkaboii@users.noreply.github.com>
2023-02-19 13:19:32 +0600

    String Formatting

---
+++ tsis1/10.py
@@ -0,0 +1,22 @@
+price = 49
+txt = "The price is {} dollars"
+print(txt.format(price))
+
+Format the price to be displayed as a number with two decimals:
+
+txt = "The price is {:.2f} dollars"
+
+quantity = 3
+itemno = 567
+price = 49
+myorder = "I want {} pieces of item number {} for {:.2f} dollars."
+print(myorder.format(quantity, itemno, price))
+
+quantity = 3
+itemno = 567
+price = 49
+myorder = "I want {0} pieces of item number {1} for {2:.2f} dollars."
+print(myorder.format(quantity, itemno, price))
+
+myorder = "I have a {carname}, it is a {model}."
+print(myorder.format(carname = "Ford", model = "Mustang"))
