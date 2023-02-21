7c1a1fd4cd7930ff200c494ca455599eaa5a62a4
Adishkaboii <123965200+Adishkaboii@users.noreply.github.com>
2023-02-21 12:14:25 +0600

    Python Dictionaries & Access Items

---
+++ tsis2/7_1.py
@@ -0,0 +1,93 @@
+Create and print a dictionary:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+
+Print the "brand" value of the dictionary:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
+
+Duplicate values will overwrite existing values:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
+print(thisdict)
+
+Print the number of items in the dictionary:
+
+print(len(thisdict))
+
+Print the data type of a dictionary:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(type(thisdict))
+
+Using the dict() method to make a dictionary:
+
+thisdict = dict(name = "John", age = 36, country = "Norway")
+print(thisdict)
+
+-----------------------
+
+Get the value of the "model" key:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = thisdict["model"]
+
+Get the value of the "model" key:
+
+x = thisdict.get("model")
+
+Get a list of the keys:
+
+x = thisdict.keys()
+
+Add a new item to the original dictionary, and see that the keys list gets updated as well:
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.keys()
+
+print(x) #before the change
+
+car["color"] = "white"
+
+print(x) #after the change
+
+Get a list of the key:value pairs
+
+  Check if "model" is present in the dictionary:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+if "model" in thisdict:
+  print("Yes, 'model' is one of the keys in the thisdict dictionary")
+x = thisdict.items()
