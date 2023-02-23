396cf7f0994e55b5168f0dcc2e76e7ad28b03834
Adishkaboii <123965200+Adishkaboii@users.noreply.github.com>
2023-02-23 18:44:52 +0600

    Create 2.py

---
+++ tsis4/math/2.py
@@ -0,0 +1,9 @@
+import math
+
+h = int(input("Height: "))
+base1 = int(input("Base, first value: "))
+base2 = int(input("Base, second value: "))
+
+area = float(((base1 + base2)/2) * h)
+
+print(area)
