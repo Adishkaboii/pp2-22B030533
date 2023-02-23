346e4d3c970a8a8f09790bda6ac408f04cfeb476
Adishkaboii <123965200+Adishkaboii@users.noreply.github.com>
2023-02-23 15:40:42 +0600

    Delete 2.py

--- tsis4/generator/2.py
+++
@@ -1,9 +0,0 @@
-def generate_evens():
-    n = int(input("Enter the number: "))
-    i = 0
-    while i <= n:
-        yield i
-        i += 2
-        
-for even in generate_evens():
-    print(even)
