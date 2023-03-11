62b9dc09256152197e2f562c047061706f6c771f
Adishkaboii <123965200+Adishkaboii@users.noreply.github.com>
2023-03-11 14:43:15 +0600

    Create 5.py

---
+++ tsis 6/built-in/5.py
@@ -0,0 +1,11 @@
+tuple1 = (1234, "1234", True)
+print(all(tuple1))
+
+tuple2 = (True, 0, "abc")
+print(all(tuple2))
+
+tuple3 = (False, 1, "abc")
+print(all(tuple3))
+
+tuple4 = (True, 123, "")
+print(all(tuple4))
