aa9df54db104003275209bbc185f0c1395cf0520
Adishkaboii <123965200+Adishkaboii@users.noreply.github.com>
2023-05-04 12:11:58 +0600

    Update 1.py

--- tsis 3/class/1.py
+++ tsis 3/class/1.py
@@ -1,1 +1,13 @@
-1
+class Printer(object):
+    def __init__(self):
+        self.String = " "
+
+    def getString(self):
+        self.String = str(input())
+
+    def printString(self):
+        print(self.String.upper())
+
+a = Printer()
+a.getString()
+a.printString()
